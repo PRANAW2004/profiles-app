4d367833a1bd8bce6f52c97dc6b7559af87a0705
PRANAW2004 <pranawrk2@gmail.com>
2025-11-07 18:12:04 +0000

    Add Like button and Increment Like without refresh

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProfileCard from './components/ProfileCard.jsx';
 import { profiles } from './data/profiles.js';
+import { useState } from 'react';
+
+
 
 export default function App() {
+const [people, setPeople] = useState(profiles);
+
+  const addLikes = (id) => {
+    setPeople(ps =>
+      ps.map(p => (p.id === id ? { ...p, likes: p.likes + 1 } : p))
+    );
+  };
+
   return (
     <Container className="py-4">
       <h1 className="mb-4 text-center">Profiles</h1>
       <Row xs={1} md={2} lg={3}>
-        {profiles.map(p => (
+        {people.map(p => (
           <Col key={p.id}>
-            <ProfileCard name={p.name} likes={p.likes} />
+            <ProfileCard name={p.name} likes={p.likes} id={p.id} onLike={() => addLikes(p.id)}/>
           </Col>
         ))}
       </Row>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,11 +1,14 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-export default function ProfileCard({ name, likes }) {
+// eslint-disable-next-line no-unused-vars
+export default function ProfileCard({ name, likes, id, onLike }) {
   return (
     <Card className="mb-3 shadow-sm">
       <Card.Body>
         <Card.Title className="h5 mb-1">{name}</Card.Title>
         <Card.Text className="mb-0">Likes: {likes}</Card.Text>
+        <Button onClick={onLike} variant="primary">Like</Button>
       </Card.Body>
     </Card>
   );
